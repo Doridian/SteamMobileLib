fd85a1f5c716a430c22a005c78450bc180c39818
Doridian Draconia <mriq91@gmail.com>
2012-02-17 07:26:48 +0100

    Added methods for adding and removing friends

---
+++ src/main/java/de/doridian/steammobile/methods/api/RemoveFriendAjax.java
@@ -0,0 +1,33 @@
+package de.doridian.steammobile.methods.api;
+
+import de.doridian.steammobile.connection.SteamConnection;
+import de.doridian.steammobile.methods.BaseWebMethod;
+import de.doridian.steammobile.methods.RequestException;
+import org.json.simple.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RemoveFriendAjax extends BaseWebMethod {
+	public RemoveFriendAjax(SteamConnection connection) {
+		connection.addSessionID(this);
+	}
+
+	private String steamid = null;
+	public void setSteamID(String steamid) {
+		this.steamid = steamid;
+	}
+
+	@Override
+	protected JSONObject doRequest(Map<String, String> data) throws RequestException {
+		if(data == null) {
+			data = new HashMap<String, String>();
+		}
+
+		if(steamid != null) {
+			data.put("steamid", steamid);
+		}
+
+		return super.doRequest(data);
+	}
+}

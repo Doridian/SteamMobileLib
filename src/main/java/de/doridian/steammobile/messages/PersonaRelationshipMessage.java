be50fe7a2031901c2b758abd77768a9f27eb5c97
Doridian Draconia <mriq91@gmail.com>
2012-03-12 13:11:46 +0100

    Ups, wrong type

--- src/main/java/de/doridian/steammobile/messages/PersonaRelationshipMessage.java
+++ src/main/java/de/doridian/steammobile/messages/PersonaRelationshipMessage.java
@@ -7,22 +7,22 @@ import java.util.Map;
 
 @Message.Type("personarelationship")
 public class PersonaRelationshipMessage extends Message {
-	public final Friend.State persona_state;
+	public final int persona_state;
 	public final String persona_name;
 	public final int status_flags;
 
 	public PersonaRelationshipMessage(JSONObject json) {
 		super(json);
-		this.persona_state = Friend.State.getByID(Integer.valueOf(json.get("persona_state").toString()));
+		this.persona_state = Integer.valueOf(json.get("persona_state").toString());
 		this.persona_name = json.get("persona_name").toString();
 		this.status_flags = Integer.valueOf(json.get("status_flags").toString());
 	}
 
-	public PersonaRelationshipMessage(String steamid_other, Friend.State persona_state, String persona_name) {
-		this(steamid_other, persona_state, persona_name, 863);
+	public PersonaRelationshipMessage(String steamid_other, int persona_state, String persona_name) {
+		this(steamid_other, persona_state, persona_name, 1);
 	}
 
-	public PersonaRelationshipMessage(String steamid_other, Friend.State persona_state, String persona_name, int status_flags) {
+	public PersonaRelationshipMessage(String steamid_other, int persona_state, String persona_name, int status_flags) {
 		super(steamid_other);
 		this.persona_state = persona_state;
 		this.persona_name = persona_name;
@@ -32,7 +32,7 @@ public class PersonaRelationshipMessage extends Message {
 	@Override
 	public void addParameters(Map<String, String> params) {
 		params.put("status_flags", "" + status_flags);
-		params.put("persona_state", "" + persona_state.getID());
+		params.put("persona_state", "" + persona_state);
 		params.put("persona_name", persona_name);
 	}
 }
